c7198f12195c45d9b1998e94fb2cc9a412d88079
osfi0001 <osfi0001@stud.kea.dk>
2024-09-02 19:49:03 +0200

    2Fashionable

---
+++ js/product.js
@@ -0,0 +1,53 @@
+//const URLparams = new URL.SearchParams(window.location.search);
+//https://kea-alt-del.dk/t7/api/products/1525
+
+fetch("https://kea-alt-del.dk/t7/api/products/1525")
+  .then((response) => response.json())
+  .then((data) => showProduct(data));
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".purchaseBox h3").textContent = product.productdisplayname;
+  document.querySelector(".productDisplayName").textContent = product.productdisplayname;
+  document.querySelector(".modelName").textContent = product.productdisplayname;
+  document.querySelector(".subCategory").textContent = product.subcategory;
+  document.querySelector(".brandName").textContent = product.brandname;
+  document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  document.querySelector(".productId").textContent = product.id;
+  document.querySelector(".baseColour").textContent = product.basecolour;
+  document.querySelector(".articleType").textContent = product.articletype;
+
+  //THESE LAST TWO DONT WANT TO WORK
+  document.querySelector(".info p").textContent = product.brandbio;
+  document.querySelector(".info h1").textContent = product.brandname;
+}
+
+/*
+id	1525
+gender	"Unisex"
+category	"Accessories"
+subcategory	"Bags"
+articletype	"Backpacks"
+basecolour	"Navy Blue"
+season	"Fall"
+productionyear	2010
+usagetype	"Casual"
+productdisplayname	"Deck Navy Blue Backpack"
+parsed	1
+soldout	0
+relid	1525
+price	1299
+discount	55
+variantname	"Deck Backpack"
+brandname	"Puma"
+brandbio	"PUMA is the World's Fastest Sports Brand"
+brandimage	"http://assets.myntassets.com/assets/images/2015/11/17/11447736932686-113016-3ff8sf.jpg"
+agegroup	"Adults-Unisex"
+colour1	"NA"
+colour2	"NA"
+fashiontype	"Fashion"
+materialcaredesc	null
+sizefitdesc	null
+description	"<p>asfafaf<br> kasjhdkashd</p>"
+styledesc	null
+*/
---
+++ js/productlist.js
@@ -0,0 +1,59 @@
+fetch("https://kea-alt-del.dk/t7/api/products")
+  .then((response) => response.json())
+  .then(showProducts);
+
+function showProducts(products) {
+  //Looper og kalder showProduct
+  products.forEach(showProduct);
+}
+
+function showProduct(product) {
+  //console.log(product);
+  //Fange template
+  const template = document.querySelector("#smallProductTemplate").content;
+  //lav en kopi
+  const copy = template.cloneNode(true);
+
+  if (product.soldout) {
+    //product er udsolgt
+    copy.querySelector("article").classList.add("soldOut");
+  }
+
+  //ændre indhold
+  copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  copy.querySelector("h3").textContent = product.productdisplayname;
+  copy.querySelector(".brandName").textContent = product.brandname;
+  copy.querySelector(".subCategory").textContent = product.subcategory;
+  copy.querySelector(".price").textContent = product.price;
+  //appende
+  document.querySelector("main").appendChild(copy);
+}
+
+/**
+  <article class="smallProduct">
+          <img src="https://kea-alt-del.dk/t7/images/webp/640/1163.webp" alt="Sahara Team India Fanwear Round Neck Jersey" />
+          <h3>Sahara Team India Fanwear Round Neck Jersey</h3>
+          <p class="subtle">Tshirts | Nike</p>
+          <p class="price"><span>Prev.</span> DKK 1595,-</p>
+          <div class="discounted">
+            <p>Now DKK 1560,-</p>
+            <p>-34%</p>
+          </div>
+          <a href="product.html">Read More</a>
+        </article>
+ */
+/**
+id	1163
+gender	"Men"
+category	"Apparel"
+subcategory	"Topwear"
+articletype	"Tshirts"
+season	"Summer"
+productionyear	2011
+usagetype	"Sports"
+productdisplayname	"Sahara Team India Fanwear Round Neck Jersey"
+price	895
+discount	null
+brandname	"Nike"
+soldout	0 
+*/
